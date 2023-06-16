37f6e017a3ca2df589c8b84464752d5fe6d6655d
vpvm96 <vpvm96@gmail.com>
2023-06-16 21:37:01 +0900

    feat: scheme static type

---
+++ backend/src/static/const.js
@@ -0,0 +1,15 @@
+const USER_POST_SCHEMA = "userSchema";
+const USER_UPDATE_SCHEMA = "userSchema";
+const USER_DELETE_SCHEMA = "userSchema";
+
+const USER_DETAIL_POST_SCHEMA = "detailUserSchema";
+
+const PRODUCT_POST_SCHEMA = "productSchema";
+
+module.exports = {
+  USER_POST_SCHEMA,
+  USER_UPDATE_SCHEMA,
+  USER_DELETE_SCHEMA,
+  USER_DETAIL_POST_SCHEMA,
+  PRODUCT_POST_SCHEMA,
+};
